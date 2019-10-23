bc79be62a4c153759d63e8066092e2ffc39b5b79
Parallels <br.fcarv@gmail.com>
2019-10-23 10:49:36 +0100

    pipeline configurations

--- electrum.py
+++ electrum.py
@@ -2,21 +2,7 @@ import os
 import sys
 import subprocess
 
-
 class ElectrumInterface(object):
 	def __init__(self,arch):
 		self.architecture = arch
-		self.setup_electrum()
 		self.results = dict()
-		self.model_check()
-
-	def setup_electrum(self):		# setup.py stuff
-		global_dir = "/home/parallels/ros/haros/haros_plugin_mc"
-		run_time_dir = os.getcwd()
-		cmd = "cp " + global_dir+ "/electrum.jar " + run_time_dir
-		os.system(cmd)
-	
-	def model_check(self):
-		with open('model.ele',"w") as f:
-			f.write(self.architecture.spec())
-		return
--- facade.py
+++ facade.py
@@ -6,8 +6,7 @@ class facade(object):
 	def __init__(self,name,nodes,topics,properties=None):
 		self.architecture = Architecture(name,nodes,topics,properties=properties)
 		self.electrum_interface = ElectrumInterface(self.architecture)
-
-
+		
 
 
 
