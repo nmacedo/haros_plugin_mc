4f35a037717c480b5fb59ae30bfebb03975e32f7
Parallels <br.fcarv@gmail.com>
2019-10-24 17:48:28 +0100

    mc parser added

---
+++ mc/ast.py
@@ -0,0 +1,123 @@
+import re
+
+# Ver como a cena dos valores esta a ser metida na estrutura, e pensar como se vai poder utilizar isto
+
+class ResultCollection():
+	def __init__(self):
+		self.results = dict()
+	def add(self,result_obj):
+		self.results.update({result_obj.property_name:result_obj})
+	
+	# Debug method: Print Results
+	def debug(self):
+		s = ""
+		for k in self.results.keys():
+			result_obj = self.results[k]
+			if isinstance(result_obj,SatResult):
+				s += "SAT\n"
+				s += result_obj.debug()
+			else:
+				s += "UNSAT\n"
+				s += result_obj.debug()
+		return s
+
+# Hierarchical object
+class ResultObject():
+	def __init__(self):
+		pass
+
+class UnsatResult(ResultObject):
+	
+	def __init__(self,t,property_name,scope):
+		self.property_type = t
+		self.property_name = property_name
+		self.scope = scope
+	
+	def debug(self):
+		s = ""
+		s += "Property Name: " + self.property_name + "\n\t"
+		s += "Property Scope: \n\t\t" + self.scope.debug() + "\n"
+		return s	
+
+class SatResult(ResultObject):
+	def __init__(self,t,property_name,scope,instance_obj):
+		self.property_type = t
+		self.property_name = property_name
+		self.scope = scope 					#Scope Object
+		self.result = instance_obj
+	
+	def debug(self):
+		s = ""
+		s += "Property Name: " + self.property_name + "\n\t"
+		s += "Property Scope: \n\t\t" + self.scope.debug() + "\n"
+		s += "\tResult:\n"
+		s += self.result.debug()
+		return s	
+
+class Scope():
+	def __init__(self,value,message,time):
+		self.value_scope = value
+		self.message_scope = message
+		self.time_scope = time
+
+	def debug(self):
+		s = ""
+		s += "Value Scope:" + str(self.value_scope) + "\n\t\t"
+		s += "Message Scope:" + str(self.message_scope) + "\n\t\t"
+		s += "Time Scope:" + str(self.time_scope) + "\n\t\t"
+		return s
+
+
+class Instance():
+	def __init__(self,states):
+		self.states = states	#[State]	
+	def debug(self):
+		s = ""
+		i = 0
+		for st in self.states:
+			s += "\t\t State: " + str(i)
+			s += "\t\t" + st.debug()
+			i = i+1
+
+		return s		
+
+
+class State():
+	def __init__(self,inbox=[],outbox=[],values=[]):
+		self.values = dict()	# dict{Message_id : value_name}
+		self.inbox = []			# node_name -> Message_id
+		self.outbox = []		# node_name -> Message_id
+		self.set_values(values)
+		self.set_state(inbox,outbox)
+
+	def remove_id(self,s):
+		return re.sub(r"\$[0-9]+","",s)
+	#[(Message_name$id, Value_name$id)] -> Void
+	def set_values(self,values):
+		for v in values:
+			message_id = v[0]
+			value_name = v[1]		# Tratar destes valores 
+			self.values.update({message_id:value_name})
+
+		
+	# Should be refactored
+	def set_state(self,inbox,outbox):
+		new_inbox = []
+		for v in inbox:
+			node_name = self.remove_id(v[0])
+			message_id = v[1]
+			new_inbox.append((node_name,message_id))
+		new_outbox = []
+		for v in outbox:
+			node_name = self.remove_id(v[0])
+			message_id = v[1]
+			new_outbox.append((node_name,message_id))
+		self.inbox = new_inbox
+		self.outbox = new_outbox
+
+	def debug(self):
+		s = ""
+		s += "Values: " + str(self.values) + "\n\n"
+		s += "inbox: " + str(self.inbox) + "\n\n"
+		s += "outbox: " + str(self.outbox) + "\n\n"
+		return s
---
+++ mc/result.py
@@ -0,0 +1,125 @@
+from antlr4 import *
+import antlr4
+from resultLexer import *
+from resultParser import *
+from resultListener import *
+from ast import *
+import re
+
+
+result_Collection = ResultCollection()
+
+def field_to_tuple(field,regex):
+	f = ''.join(field)
+	f = re.sub(regex,"",f)
+	if f != '':
+		f = f.split(',')
+	else:
+		f = []
+	new_f = []
+	for x in f:
+		aux = x.split('->')
+		aux[0] = aux[0].replace(' ','')		#replace is an outplace operator
+		aux[1] = aux[1].replace(' ','')	
+		new_f.append((aux[0] , aux[1]))
+	f = new_f
+	return f
+
+
+def ext_sig(s):
+	new_s = re.sub(r"this/","",s)
+	new_s = re.sub(r"={.*}","",new_s)
+	return new_s
+
+def search(v_id,st_txt):
+	#print("Search: " + v_id)
+	fl = filter(lambda x: v_id in x, st_txt)
+	# Testing
+	fl = filter(lambda x: 'univ' not in x, fl)
+	fl = filter(lambda x: 'this/Message' not in x, fl)
+	fl = filter(lambda x: 'this/Value' not in x, fl)
+	#print("Filtered List: " + str(fl))
+	rl = map(lambda x : ext_sig(x),fl)
+	#print("Result: " + str(rl))
+	return rl
+
+def transform(values,lines):
+	new_values = []
+	for v in values:
+		m_id = v[0]
+		v_id = v[1]
+		abs_l = search(v_id,lines)
+		new_values.append((m_id,abs_l))
+	return new_values
+
+# String -> State
+def state_from_description(txt):
+	lines = txt.split('\n')	
+	# Inbox
+	inbox = field_to_tuple(filter(lambda x: 'this/Node<:inbox' in x,lines),
+						"(this/Node<:inbox={)|}")
+	# Outbox
+	outbox = field_to_tuple(filter(lambda x: 'this/Node<:outbox' in x,lines),
+						"(this/Node<:outbox={)|}")	
+	# Values
+	values = field_to_tuple(filter(lambda x: 'this/Message<:value' in x,lines),
+						"(this/Message<:value={)|}")
+	values = transform(values,lines)
+	# DEBUG
+	#print("Inbox :" + str(inbox))
+	#print("Outbox :" + str(outbox))
+	#print("Values :" + str(values))
+	state_obj = State(inbox=inbox,outbox=outbox,values=values)
+	return state_obj
+	
+class Interpreter(resultListener):
+	 
+	 def exitCommand(self, ctx):
+	 	# name interpretation
+	 	name_rule = ctx.name()
+	 	name = str(name_rule.NAME()) 			#Property Name
+	 	t = str(name_rule.TYPE())				#Property Time
+	 	scopes = re.findall(r'\d+',str(name_rule.SCOPE())) #Property Scopes
+	 	time_scope = scopes[1]
+	 	value_scope = scopes[3]
+	 	message_scope = scopes[4]
+	 											# Result interpretation and AST Object Creation
+	 	scope_obj = Scope(value_scope,message_scope,time_scope)
+	 	result_obj = None
+	 	result_rule = None
+	 	if ctx.result().unsat() is not None:	# UNSAT Object
+	 		result_rule = ctx.result().unsat()
+	 		result_obj = UnsatResult(t,name,scope_obj)
+	 	else:									# SAT Object
+	 		results_txt = []
+	 		states = []							# [State]
+	 		result_rule = ctx.result().sat()
+	 		result_description = result_rule.sts()
+	 		while(result_description is not None):
+	 			description = str(result_description.st().descriptions().FIELDESCRIPTION())
+	 			state_obj = state_from_description(description)
+	 			states.append(state_obj)			
+	 			# Move Foward
+	 			result_description = result_description.sts()		
+	 		instance_obj = Instance(states)
+	 		result_obj = SatResult(t,name,scope_obj,instance_obj)
+
+	 	result_Collection.add(result_obj)					# Update Collection
+
+
+
+def parse():
+	with open("teste.txt") as f:
+		data = f.read()
+	inputStream = antlr4.InputStream(data)
+	lexer = resultLexer(inputStream)
+	stream = antlr4.CommonTokenStream(lexer)
+	parser = resultParser(stream)
+	tree = parser.gr()
+	interpreter = Interpreter()
+	walker = ParseTreeWalker()
+	walker.walk(interpreter,tree)
+
+
+if __name__ == '__main__':
+	parse()
