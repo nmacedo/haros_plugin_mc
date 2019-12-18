0fef17d095c5b3dbdaecaf32736b1b24d21b204e
Parallels <br.fcarv@gmail.com>
2019-12-18 15:03:18 +0000

    haros.yaml update

--- plugin.py
+++ plugin.py
@@ -3,14 +3,15 @@ from mc_Interface import *
 
 def configuration_analysis(iface, scope):
 	if scope.nodes.__len__() <= 1:	# Node Configurations
+		print("Node configuration.")
 		return
-	print("[MC] Running analysis...")
-	#mc_iface = MC_Interface(scope.name, scope.nodes, scope.topics,properties=scope.hpl_properties)
-	#results = mc_iface.model_check()
-	#html_list = mc_iface.to_html(results)
+	
+	mc_iface = MC_Interface(scope.name, scope.nodes, scope.topics,properties=scope.hpl_properties)
+	results = mc_iface.model_check()
+	html_list = mc_iface.to_html(results)
 	print("[MC] Reporting results...")
-	#for s in html_list:
-	#	if s is None:
-	#		continue
-	#	else:
-	#		iface.report_violation("mc",s)
+	for s in html_list:
+		if s is None:
+			continue
+		else:
+			iface.report_violation("mc",s)
